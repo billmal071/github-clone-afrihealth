ee850617bd12258526bd629f0770fc395303ec17
billmal071 <billmal071@gmail.com>
2021-11-11 03:55:04 +0100

    fix:token issues, switching to process.env

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,10 +1,13 @@
 import { GraphQLClient } from "graphql-request";
 
 export async function fetcher(query: string, variables: any) {
+  const TOKEN = import.meta.env.DEV
+    ? import.meta.env.VITE_GITHUB_TOKEN
+    : process.env.VITE_GITHUB_TOKEN;
   const client = new GraphQLClient("https://api.github.com/graphql", {
     headers: {
-      authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`
-    }
+      authorization: `Bearer ${TOKEN}`,
+    },
   });
 
   return await client.request(query, variables);
